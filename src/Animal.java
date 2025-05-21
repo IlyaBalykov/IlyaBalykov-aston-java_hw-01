3abddc27fdeca281687ca3de984a3f123c35ec7b
Ilya Balykov <balykov.ilya.88@gmail.com>
2025-05-21 23:21:42 +0300

    refactor: change instance paramters

--- src/Animal.java
+++ src/Animal.java
@@ -15,27 +15,24 @@ public abstract class Animal {
 
     public static void main(String[] args) {
 
-        Mammal polarBear = new Bear("Misha");
+        Mammal polarBear = new Bear("Misha", "white");
         polarBear.getName();
         polarBear.getFurColor();
         polarBear.hasSpine();
 
-        Mammal persianCat = new Cat("Barsik", true);
+        Mammal persianCat = new Cat("Barsik", "black");
         persianCat.getName();
         persianCat.getFurColor();
         persianCat.hasSpine();
-        ;
 
         Mammal blueWhale = new Whale("Whale");
         blueWhale.getName();
         blueWhale.getFurColor();
-        ;
         blueWhale.hasSpine();
-        ;
 
         Fish goldenFish = new Fish("Gold");
         goldenFish.getName();
         goldenFish.getLivesInWater();
-        goldenFish.getHasSpine();
+        goldenFish.hasSpine();
     }
 }
